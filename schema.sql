e7380f589aaca6d1ecd76a3e807f4320064d59d8
WasteOfADrumBum <jmsmall89@gmail.com>
2020-09-26 11:14:20 -0400

    INITIAL CODE UPDATE

--- schema.sql
+++
@@ -1,4 +0,0 @@
--- Drops the projectdata if it exists currently --
-DROP DATABASE IF EXISTS projectdata;
--- Creates the "projectdata" database --
-CREATE DATABASE projectdata;
--- server.js
+++ server.js
@@ -1,31 +1,63 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080; // http://localhost:9080/
+require('dotenv').config();
+const express = require('express');
+const exphbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const passport = require('passport');
+const moment = require('moment');
+const helmet = require('helmet');
+const PORT = process.env.PORT || 3333;
+const app = express();
+const db = require('./models');
 
-// Requiring our models for syncing
-var db = require("./models");
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-// Static directory
-app.use(express.static("public"));
+if (app.get('env') !== 'test') {
+  app.use(morgan('dev')); // Hook up the HTTP logger
+}
 
-// Routes
-// =============================================================
-require("./routes/html-routes.js")(app);
-require("./routes/author-api-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
+app.use(express.static('public'));
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({ force: true }).then(function () {
-	app.listen(PORT, function () {
-		console.log("App listening on PORT " + PORT);
-	});
+require('./config/passport')(db, app, passport); // pass passport for configuration
+
+// Define our routes
+app.use('/api', require('./routes/apiRoutes')(passport, db));
+app.use(require('./routes/htmlRoutes')(db));
+
+// Secure express app
+app.use(helmet.hsts({
+  maxAge: moment.duration(1, 'years').asMilliseconds()
+}));
+
+// catch 404 and forward to error handler
+if (app.get('env') !== 'development') {
+  app.use((req, res, next) => {
+    const err = new Error('Not Found: ' + req.url);
+    err.status = 404;
+    next(err);
+  });
+}
+
+const syncOptions = {
+  force: process.env.FORCE_SYNC === 'true'
+};
+
+if (app.get('env') === 'test') {
+  syncOptions.force = true;
+}
+
+db.sequelize.sync(syncOptions).then(() => {
+  if (app.get('env') !== 'test' && syncOptions.force) {
+    require('./db/seed')(db);
+  }
+
+  app.listen(PORT, () => {
+    console.log(`App listening on port: ${PORT}`);
+  });
 });
+
+module.exports = app;
