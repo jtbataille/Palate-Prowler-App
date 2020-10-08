cf5257bb7100f339f50ce71525ff175686aa3825
Joshua Small <jmsmall89@gmail.com>
2020-10-08 10:21:21 -0400

    FIXES
    Fixed image paths on main page and details page, updated seed, fixed div tags, updated to mobile firnedly responsiveness, created responsive navbar, fixed container col ressponsiveness, updated h tags, removed inline styling  updated event container button divs and format

--- db/seed.js
+++ db/seed.js
@@ -22,7 +22,7 @@ module.exports = (db) => {
 				state: "North Carolina",
 				zip: "28410",
 				about: "Here at 'Adam's Appetizing Eats' we strive to provide the best possible meal for you while keeping the pandemic in mind. Come eat with us as we make a safe dining experience with good food and safe company!",
-				picture: "/assets/images/adam-demo.jpg",
+				picture: "assets/images/adam-demo.jpg",
 				value: false,
 				UserId: 1,
 			}).then(() => {
@@ -35,7 +35,7 @@ module.exports = (db) => {
 					state: "North Carolina",
 					zip: "27599",
 					about: "Uma's Truck is fabulous with its array of cuisine to meet any of your various needs--all with a vegan twist! She will make you wonder if you're even able to tell that her ragù or filet is 100% animal-free--not to mention her tantalizing desserts! Come check us out!",
-					picture: "/assets/images/uma-demo.jpg",
+					picture: "assets/images/uma-demo.jpg",
 					value: false,
 					UserId: 2,
 				});
